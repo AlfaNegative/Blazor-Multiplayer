b33f583b685a6939f27746ea71ac97fa5223da5b
unknown <kasen.meyerhoffer@engineering.snow>
2025-11-04 15:14:36 -0700

    First Commit

---
+++ Blazor/Game.cs
@@ -0,0 +1,25 @@
+using System.Timers;
+
+public class Game
+{
+    public int TickNumber { get; set; }
+    private System.Timers.Timer? tickTimer = null;
+    public void DoTick(object sender, ElapsedEventArgs e)
+    {
+        TickNumber++;
+        Console.WriteLine("Did Tick" + TickNumber);
+    }
+    public void StartGame()
+    {
+        tickTimer = new System.Timers.Timer(1000);
+        tickTimer.Elapsed += DoTick;
+        tickTimer.AutoReset = true;
+        tickTimer.Enabled = true;
+    }
+
+    ~Game()
+    {
+        tickTimer?.Stop();
+        tickTimer.Dispose();
+    }
+}
