4aead07effdc0eb1f203337cf29c9660ac4dfc97
unknown <kasen.meyerhoffer@ENGINEERING.SNOW>
2025-11-04 15:23:58 -0700

    Second Commit

--- Blazor/Game.cs
+++ Blazor/Game.cs
@@ -2,12 +2,15 @@ using System.Timers;
 
 public class Game
 {
+    public event Action? GameUpdated;
     public int TickNumber { get; set; }
     private System.Timers.Timer? tickTimer = null;
     public void DoTick(object sender, ElapsedEventArgs e)
     {
         TickNumber++;
-        Console.WriteLine("Did Tick" + TickNumber);
+        Console.WriteLine("Did Tick " + TickNumber);
+
+        GameUpdated?.Invoke();
     }
     public void StartGame()
     {
@@ -18,9 +21,9 @@ public class Game
         tickTimer.Enabled = true;
     }
 
-    ~Game()
-    {
-        tickTimer?.Stop();
-        tickTimer.Dispose();
-    }
+    // ~Game()
+    // {
+    //     tickTimer?.Stop();
+    //     tickTimer.Dispose();
+    // }
 }
